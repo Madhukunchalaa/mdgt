fccfa084091c8dad5f1920a3e76ff50fb56bc6b0
madhu kunchala <madhkunchala@gmail.com>
2026-04-17 10:34:28 +0530

    Add URL aliases for uploads and configure CSRF_TRUSTED_ORIGINS to resolve CORS issues

--- meil_backend/uploads/urls.py
+++ meil_backend/uploads/urls.py
@@ -4,8 +4,12 @@ from . import views
 urlpatterns = [
     path('bulk-upload/', views.bulk_upload, name='bulk_upload'),
     path('bulk-upload', views.bulk_upload),
+    path('bulk_upload/', views.bulk_upload),
+    path('bulk_upload', views.bulk_upload),
     path('get-fields/', views.get_model_fields, name='get_model_fields'),
     path('get-fields', views.get_model_fields),
+    path('get_fields/', views.get_model_fields),
+    path('get_fields', views.get_model_fields),
     path('download-template/', views.generate_excel_template, name='generate_excel_template'),
     path('download-template', views.generate_excel_template),
 ]
